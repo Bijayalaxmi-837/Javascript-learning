3b338c25eeff502b6eee9ff92a6a72b246cce65d
Bijayalaxmi-837 <bijayalaxmis837@gmail.com>
2024-11-01 19:36:22 +0530

    01 push
    01 push

--- 01.js
+++ 01.js
@@ -1,3 +1,19 @@
 var city;
 city = "ctc";
 console.log("city:", city );
+
+var bestfriend;
+bestfriend = "puja";
+console.log("bestfriend:" ,bestfriend);
+
+var country;
+country = "india";
+console.log("country:", country);
+
+var SchoolName;
+SchoolName = "sidheswara High School";
+console.log("SchoolName:", SchoolName);
+
+var CollegeName;
+CollegeName = "L.N College,Korua,Patkura";
+console.log( "CollegeName:",  CollegeName);
