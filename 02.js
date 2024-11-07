86ef3fe55c89f79c793bd59a03abef20e8677239
Bijayalaxmi-837 <bijayalaxmis837@gmail.com>
2024-11-07 18:44:46 +0530

    02 js
    02 js

---
+++ 02.js
@@ -0,0 +1,23 @@
+// data type
+var fullName = "BS Swain";
+console.log("fullName:", fullName);
+var typeoffullName = typeof fullName;
+console.log("type of variable fullName is:", typeoffullName);
+
+var age = 33;
+console.log("Age :" , age,  " type of age is: ", typeof age);
+ 
+var totalmoney = 400000;
+var weight = 65;
+var height = "6fit";
+console.log("height:", height,"type of height is:",typeof height);
+
+var ismarried = true;
+console.log("ismarried:", ismarried, "type of ismarried is:" , typeof ismarried );
+ 
+var totalexp;
+console.log("totalexp:", totalexp, "type of totalexp is:" , typeof totalexp);
+
+var skill = null;
+console.log("skill:", skill, "type of skill is:" , typeof skill);
+
