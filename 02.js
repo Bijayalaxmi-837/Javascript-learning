072cc8e847784910b7fd99425362727266226790
Bijayalaxmi-837 <bijayalaxmis837@gmail.com>
2024-11-07 19:46:02 +0530

    02 js
    02 js

--- 02.js
+++ 02.js
@@ -21,3 +21,31 @@ console.log("totalexp:", totalexp, "type of totalexp is:" , typeof totalexp);
 var skill = null;
 console.log("skill:", skill, "type of skill is:" , typeof skill);
 
+
+// Assignments
+var myfullname;
+console.log("Before initialization:" , myfullname);
+
+myfullname = "B S";
+console.log("After initialization:" , myfullname);
+
+myfullname = "B L S";
+console.log("my complete name is:" , myfullname);
+
+var myLove = "I Love Only Js";
+console.log("myLove:" , myLove);
+
+var firstname = "Bijayalaxmi";
+console.log("firstname:" , firstname);
+var Lastname = "Swain";
+console.log("Lastname:" , Lastname);
+var Age = 25;
+console.log("Age:" , Age);
+var Address= "Bangari,Bamara,Garadpur,Kendrapara";
+console.log("Address:" , Address);
+var Pincode = 754153;
+console.log("Pincode:" , Pincode);
+var Gender = "Female";
+console.log("Gender:",Gender);
+var ismarried = "yes";
+console.log("ismarried:",ismarried);
