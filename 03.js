b53995853261337fd5f28a98e89a355d9ec0a3a0
Bijayalaxmi-837 <bijayalaxmis837@gmail.com>
2024-11-21 19:44:51 +0530

    03 js
    03 js

---
+++ 03.js
@@ -0,0 +1,39 @@
+// function
+function sayHello(){
+    console.log("heysony");
+}
+sayHello();
+console.log("------------------")
+function  helloWorld(){
+    console.log("welcome");
+}
+helloWorld();
+console.log("------------------")
+
+function  dear(){
+    console.log("friend");
+}
+dear();
+console.log("------------------");
+
+ function personalDetails(firstname, Lastname, CollegeName){
+    console.log("firstName is:bijayaLaxmi");
+    console.log("lasstName is:swain");
+    console.log("collegeNmae is:L.N");
+ }
+ personalDetails();
+ console.log("------------------");
+ function Details(para1, para2, para3){
+    console.log("Have a");
+    console.log("Nice");
+    console.log("Day");
+ }
+Details();
+ console.log("------------------");
+
+ function addThreeValue(n1, n2, n3){
+     var result = n1+n2+n3;
+     console.log("addition Of Three is: result");
+     
+ }
+ addThreeValue(10, 20, 30, 40);
